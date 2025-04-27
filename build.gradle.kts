62a6121a87ed64d5714a189c3e1c69005945e39f
ATPStorages <tcgytsc@gmail.com>
2025-04-27 17:52:59 -0500

    BSL update.

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 group = "org.bread_experts_group"
-version = "1.2.1"
+version = "1.2.2"
 
 repositories {
 	mavenCentral()
@@ -18,7 +18,7 @@ repositories {
 }
 
 dependencies {
-	implementation("org.bread_experts_group:bread_server_lib-code:1.3.0-SNAPSHOT")
+	implementation("org.bread_experts_group:bread_server_lib-code:1.3.0")
 }
 
 tasks.test {
